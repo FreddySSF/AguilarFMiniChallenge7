d37a9da8b9093e0a34294d15834c7086fb807a64
FreddySSF <fernandoaguilarssf@gmail.com>
2022-10-21 11:49:35 -0700

    AguilarF MiniChallenge7 Endpoint Update

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // Mini Challenge 7 Reverse It Endpoint
 // Create a program that asks the user for a number and reverses the number for them.
 // Include option to replay and data validation.
-// Peer Reviewed By:
+// Peer Reviewed By: Christopher Prado - Your program runs very smoothly and code is very cleanly organized and easy to follow. I also like how personable your messages were for the user.
 
 Console.Clear();
 
